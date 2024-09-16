fe23a8b95edb5137f16587b87af383b653581732
alicemorey <alicemmorey@gmail.com>
2024-09-16 17:21:25 +0200

    Update CustomActions.js

---
+++ components/CustomActions.js
@@ -0,0 +1,10 @@
+import { TouchableOpacity } from "react-native";
+
+const CustomActions = () => {
+  return (
+    <TouchableOpacity style={styles.container}>
+    </TouchableOpacity>
+  );
+}
+
+export default CustomActions;
